9278015275413fa67fdb60b8f424afdb2a75d30f
Shreeya Parvatham <82877134+Shreeya-167@users.noreply.github.com>
2024-01-29 01:25:32 +0530

    Update register.js

--- client/templates/auth/register.js
+++ client/templates/auth/register.js
@@ -5,12 +5,10 @@ Template.register.events({
       const password = event.target.password.value;
       const role = event.target.role.value;
   
-      // Validate email, password, and role
-  
-      // Create user
+    
       const userId = Accounts.createUser({ email, password });
   
-      // Set user roles
+      
       Users.update(userId, { $set: { roles: [role] } });
     },
   });
